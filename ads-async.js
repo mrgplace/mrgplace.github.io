03b9f8ccd14a478016977314171b09309fe58811
Albert Bagdasaryan <albert.bagd@gmail.com>
2021-06-22 08:43:52 +0300

    Improved code style and comments.

---
+++ ads-async.js
@@ -0,0 +1,5 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = 'https://r.mradx.net/img/BD/04C5EF.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+})();
